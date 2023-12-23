940ff997bad034c5aed7aedacd6af4bd2b752194
César Zárate <cesarjzo@outlook.com>
2023-12-22 22:12:36 -0600

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import Head from './Head'
-import DynamicContent from './DynamicContent'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    const message = `count is ${count}`
-    console.log(message)
-  }, [count])
-
   return (
     <>
-      <div className='logos'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Head text='Vite + React' />
-      <DynamicContent showSecret={false} />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
