20ba590a53cab4852a92ff473adef9b18f9652cb
César Zárate <cesarjzo@outlook.com>
2023-12-25 20:51:37 -0600

    Add react-leaflet Marker component and implement map click event handling

--- src/utils/Map.tsx
+++ src/utils/Map.tsx
@@ -1,9 +1,11 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-
+import { MapContainer, TileLayer, useMapEvent } from "react-leaflet";
+import { Marker as M } from "react-leaflet";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/leaflet.css";
+import { coordsDTO } from "./coords.model";
+import { useState } from "react";
 
 const DefaultIcon = L.icon({
   iconUrl: icon,
@@ -14,16 +16,21 @@ const DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 export default function Map({ height }: MapProps) {
+  const [coords, setCoords] = useState<coordsDTO[]>([]);
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      style={{ height: height }}
-    >
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: height }}>
       <TileLayer
         attribution="React Movies"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      /> 
+      />
+      <MapClick
+        setCoords={(coords) => {
+          setCoords([coords]);
+        }}
+      />
+      {coords.map((coords, index) => (
+        <Marker key={index} coords={coords} />
+      ))}
     </MapContainer>
   );
 }
@@ -35,3 +42,22 @@ export interface MapProps {
 Map.defaultProps = {
   height: "500px",
 };
+
+function MapClick({ setCoords }: MapClickProps) {
+  useMapEvent("click", (e) => {
+    setCoords({ lat: e.latlng.lat, lng: e.latlng.lng });
+  });
+  return null;
+}
+
+export interface MapClickProps {
+  setCoords: (coords: coordsDTO) => void;
+}
+
+function Marker({ coords }: MarkerProps) {
+  return <M position={[coords.lat, coords.lng]}></M>;
+}
+
+interface MarkerProps {
+  coords: coordsDTO;
+}
---
+++ src/utils/coords.model.d.ts
@@ -0,0 +1,4 @@
+export interface coordsDTO {
+    lng: number;
+    lat: number;
+}
