f2207338f876cddbf8b3d2d17b5bb157525b1e13
César Zárate <cesarjzo@outlook.com>
2023-12-25 19:57:53 -0600

    Add TheatersForm component to CreateTheater and UpdateTheater

--- src/theaters/CreateTheater.tsx
+++ src/theaters/CreateTheater.tsx
@@ -1,7 +1,14 @@
+import TheatersForm from "./TheatersForm";
+
 export default function CreateTheater() {
   return (
     <>
       <h2>Create Theater</h2>
+
+      <TheatersForm
+        model={{ name: "" }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
--- src/theaters/UpdateTheater.tsx
+++ src/theaters/UpdateTheater.tsx
@@ -1,7 +1,14 @@
+import TheatersForm from "./TheatersForm";
+
 export default function UpdateTheater() {
   return (
     <>
       <h2>Update Theater</h2>
+
+      <TheatersForm
+        model={{ name: "Cinema City" }}
+        onSubmit={(values) => console.log(values)}
+      />
     </>
   );
 }
