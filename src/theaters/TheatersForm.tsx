71be27c909bc2798566005b99488066761b5c123
César Zárate <cesarjzo@outlook.com>
2023-12-25 19:51:53 -0600

    Add form components to TheatersForm.tsx

--- src/theaters/TheatersForm.tsx
+++ src/theaters/TheatersForm.tsx
@@ -1,19 +1,32 @@
-import { Formik, FormikHelpers } from "formik";
+import {Form, Formik, FormikHelpers} from "formik";
+import {Link} from "react-router-dom";
 import * as Yup from "yup";
+import FormGroupText from "../utils/FormGroupText.tsx";
+import Button from "../utils/Button.tsx";
 
-export default function TheatersForm({ model, onSubmit }: TheatersFormProps) {
-  return (
-    <Formik
-      initialValues={model}
-      onSubmit={onSubmit}
-      
-    >
-      
-    </Formik>
-  );
+export default function TheatersForm({model, onSubmit}: TheatersFormProps) {
+    return (
+        <Formik
+            initialValues={model}
+            onSubmit={onSubmit}
+            validationSchema={Yup.object({
+                name: Yup.string().required("This field is required").firstLetterUppercase()
+            })}
+        >
+            {(formikProps) => (
+                <Form>
+                    <FormGroupText field="name" label="Name"/>
+                    <Button disabled={formikProps.isSubmitting} type="submit">
+                        Save
+                    </Button>
+                    <Link to="/theaters">Cancel</Link>
+                </Form>
+            )}
+        </Formik>
+    );
 }
 
 export interface TheatersFormProps {
-  model: theaterCreationDTO;
-  onSubmit: (values: theaterCreationDTO, actions: FormikHelpers<theaterCreationDTO>) => void;
+    model: theaterCreationDTO;
+    onSubmit: (values: theaterCreationDTO, actions: FormikHelpers<theaterCreationDTO>) => void;
 }
