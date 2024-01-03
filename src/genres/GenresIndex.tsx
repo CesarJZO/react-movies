a6bac9d946a493b94de94ec57df8d60020cf79fb
César Zárate <cesarjzo@outlook.com>
2024-01-03 14:03:35 -0600

    Add ErrorPrinter component and implement deleteGenre function

--- src/genres/GenresIndex.tsx
+++ src/genres/GenresIndex.tsx
@@ -6,6 +6,7 @@ import { urlGenres } from "../utils/endpoints";
 import GenericList from "../utils/GenericList";
 import Button from "../utils/Button";
 import Pagination from "../utils/Pagination";
+import ErrorPrinter from "../utils/ErrorPrinter";
 
 export default function GenresIndex() {
   const defaultRecordsPerPage = 10;
@@ -15,26 +16,42 @@ export default function GenresIndex() {
   const [recordsPerPage, setRecordsPerPage] = useState(defaultRecordsPerPage);
   const [currentPage, setCurrentPage] = useState(1);
 
+  const [errors, setErrors] = useState<string[]>([]);
+
   useEffect(() => {
-    axios
-      .get(urlGenres, {
-        params: {
-          page: currentPage,
-          recordsPerPage: recordsPerPage,
-        },
-      })
-      .then((response: AxiosResponse<genreDTO[]>) => {
-        const totalRecords = parseInt(
-          response.headers["totalamountofrecords"],
-          10
-        );
-
-        setTotalAmountOfRecords(Math.ceil(totalRecords / recordsPerPage));
-
-        console.log(response.data);
-        setGenres(response.data);
-      });
+    loadData();
   }, [currentPage, recordsPerPage]);
+
+  const deleteGenre = async (id: number) => {
+    try {
+      await axios.delete(`${urlGenres}/${id}`);
+      loadData();
+    }
+    catch (error) {
+      setErrors((error as any).response.data);
+    }
+  }
+
+  function loadData() {
+    axios
+    .get(urlGenres, {
+      params: {
+        page: currentPage,
+        recordsPerPage: recordsPerPage,
+      },
+    })
+    .then((response: AxiosResponse<genreDTO[]>) => {
+      const totalRecords = parseInt(
+        response.headers["totalamountofrecords"],
+        10
+      );
+
+      setTotalAmountOfRecords(Math.ceil(totalRecords / recordsPerPage));
+
+      console.log(response.data);
+      setGenres(response.data);
+    });
+  }
 
   return (
     <>
@@ -53,6 +70,8 @@ export default function GenresIndex() {
         <option>5</option>
         <option>10</option>
       </select>
+
+      <ErrorPrinter errors={errors} />
 
       <Pagination
         currentPage={currentPage}
@@ -73,7 +92,9 @@ export default function GenresIndex() {
               <tr key={genre.id}>
                 <td>
                   <Link to={`/genres/update/${genre.id}`}>Edit</Link>
-                  <Button>Delete</Button>
+                  <Button
+                    onClick={() => deleteGenre(genre.id)}
+                  >Delete</Button>
                 </td>
                 <td>{genre.name}</td>
               </tr>
