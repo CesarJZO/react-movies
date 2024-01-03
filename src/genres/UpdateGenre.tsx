a6548ecfca5bc598d8912d52144b3cfdf3ecb193
César Zárate <cesarjzo@outlook.com>
2024-01-03 14:52:45 -0600

    Refactor UpdateGenre component

--- src/genres/UpdateGenre.tsx
+++ src/genres/UpdateGenre.tsx
@@ -1,49 +1,23 @@
-import { useNavigate, useParams } from "react-router-dom";
-import GenresForm from "./GenresForm";
-import { useEffect, useState } from "react";
-import axios, { AxiosResponse } from "axios";
+import UpdateEntity from "../utils/UpdateEntity";
 import { urlGenres } from "../utils/endpoints";
-import { genreCreationDTO } from "./genres.model";
-import Loading from "../utils/Loading";
-import ErrorPrinter from "../utils/ErrorPrinter";
+import GenresForm from "./GenresForm";
+import { genreCreationDTO, genreDTO } from "./genres.model";
 
 export default function UpdateGenre() {
-  const { id } = useParams();
-
-  const [genre, setGenre] = useState<genreCreationDTO>();
-  const [errors, setErrors] = useState<string[]>([]);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    axios
-      .get(`${urlGenres}/${id}`)
-      .then((res: AxiosResponse<genreCreationDTO>) => setGenre(res.data));
-  }, []);
-
-  const edit = async (genre: genreCreationDTO) => {
-    try {
-      await axios.put(`${urlGenres}/${id}`, genre);
-      navigate("/genres");
-    } catch (error) {
-      setErrors((error as any).response.data);
-    }
-  };
-
   return (
     <>
-      <h2>Update Genre {id}</h2>
-
-      <ErrorPrinter errors={errors} />
-
-      {genre ? (
-        <GenresForm
-          model={genre}
-          onSubmit={async (values) => await edit(values)}
-        />
-      ) : (
-        <Loading />
-      )}
+      <UpdateEntity<genreCreationDTO, genreDTO>
+        url={urlGenres}
+        indexUrl="/genres"
+        entityName="Genre"
+      >
+        {(genre, edit) => (
+          <GenresForm
+            model={genre}
+            onSubmit={async (values) => await edit(values)}
+          />
+        )}
+      </UpdateEntity>
     </>
   );
 }
---
+++ src/utils/UpdateEntity.tsx
@@ -0,0 +1,60 @@
+import { ReactElement, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios, { AxiosResponse } from "axios";
+import ErrorPrinter from "./ErrorPrinter";
+import Loading from "./Loading";
+
+export default function UpdateEntity<TCreation, TRead>({
+  url,
+  indexUrl,
+  entityName,
+  children,
+  transform,
+}: UpdateEntityProps<TCreation, TRead>) {
+  const { id } = useParams();
+
+  const [entity, setEntity] = useState<TCreation>();
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${url}/${id}`)
+      .then((res: AxiosResponse<TRead>) => setEntity(transform(res.data)));
+  }, []);
+
+  const edit = async (entity: TCreation) => {
+    try {
+      await axios.put(`${url}/${id}`, entity);
+      navigate(indexUrl);
+    } catch (error) {
+      setErrors((error as any).response.data);
+    }
+  };
+
+  return (
+    <>
+      <h2>Update {entityName}</h2>
+
+      <ErrorPrinter errors={errors} />
+
+      {entity ? children(entity, edit) : <Loading />}
+    </>
+  );
+}
+
+interface UpdateEntityProps<TCreation, TRead> {
+  url: string;
+  indexUrl: string;
+  entityName: string;
+  children: (
+    entity: TCreation,
+    edit: (entity: TCreation) => void
+  ) => ReactElement;
+  transform: (entity: TRead) => TCreation;
+}
+
+UpdateEntity.defaultProps = {
+  transform: (entity: any) => entity,
+};
