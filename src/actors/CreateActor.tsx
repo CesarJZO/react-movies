e4715de8bc02acbd9e465f51b4bdc796b98a5801
César Zárate <cesarjzo@outlook.com>
2024-01-03 17:12:46 -0600

    Add actor creation functionality and error handling

--- src/actors/CreateActor.tsx
+++ src/actors/CreateActor.tsx
@@ -1,13 +1,34 @@
+import axios from "axios";
 import ActorsForm from "./ActorsForm";
+import { actorCreationDTO } from "./actors.model";
+import { urlActors } from "../utils/endpoints";
+import { useNavigate } from "react-router-dom";
+import ErrorPrinter from "../utils/ErrorPrinter";
+import { useState } from "react";
 
 export default function CreateActor() {
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState<string[]>([]);
+
+  async function create(actor: actorCreationDTO) {
+    try {
+      await axios.post(urlActors, actor);
+      navigate("/actors");
+    } catch (error) {
+      setErrors((error as any).response.data);
+    }
+  }
+
   return (
     <>
       <h2>Create Actor</h2>
 
+      <ErrorPrinter errors={errors} />
+
       <ActorsForm
         model={{ name: "", dateOfBirth: undefined! }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={async (values) => await create(values)}
       />
     </>
   );
--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,6 +1,11 @@
 const apiURL = import.meta.env.VITE_API_URL;
 
 /**
- * The URL of the API endpoint for the list of genres.
+ * The URL of the API endpoint for the genres' controller.
  */
 export const urlGenres = `${apiURL}/api/genres`;
+
+/**
+ * The URL of the API endpoint for the actors' controller.
+ */
+export const urlActors = `${apiURL}/api/actors`;
