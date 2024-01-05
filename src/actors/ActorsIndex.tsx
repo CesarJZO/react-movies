9b4ee6d6ad19e7597f722e640d78389b1ef19ee7
César Zárate <cesarjzo@outlook.com>
2024-01-05 16:50:41 -0600

    Add create actor functionality and display actors in ActorsIndex

--- src/actors/ActorsIndex.tsx
+++ src/actors/ActorsIndex.tsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+import EntityIndex from "../utils/EntityIndex";
+import { urlActors } from "../utils/endpoints";
+import { actorDTO } from "./actors.model";
+
 export default function ActorsIndex() {
   return (
     <>
-      <h2>Actors</h2>
+      <Link to="/actors/create">Create Actor</Link>
+      
+      <EntityIndex<actorDTO>
+        url={urlActors}
+        creatingUrl="/actors/create"
+        title="Actors"
+        entityName="Actor"
+      >
+        {(actors, buttons) => (
+          <>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Date of Birth</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {actors?.map((actor) => (
+                <tr key={actor.id}>
+                  <td>
+                    {buttons(`/actors/update/${actor.id}`, actor.id)}
+                  </td>
+                  <td>
+                    {actor.name}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </>
+        )}
+      </EntityIndex>
     </>
   );
 }
